ee9f7760a264a9b2b6a5cc3b06d57b204b6602b9
Drien BRETON <dbreton@e1r2p12.42.fr>
2016-05-23 14:37:44 +0200

    added obj array, segfault

--- parse.c
+++ parse.c
@@ -6,7 +6,7 @@
 /*   By: dbreton <dbreton@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/16 12:45:34 by dbreton           #+#    #+#             */
-/*   Updated: 2016/05/23 14:23:07 by dbreton          ###   ########.fr       */
+/*   Updated: 2016/05/23 14:35:25 by dbreton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,37 @@ static void		body_parse(t_mlx *s, int fd, int *l)
 	char		*line;
 	char		**tmp;
 	int			tot_len;
+	int			o;
 
 	tot_len = s->obj_len + s->src_len;
+	o = 0;
 	while (*l < tot_len && get_next_line(fd, &line) >= 0 )
 		if (line[0] != '#')
 		{
 			tmp = ft_strsplit(line,  ' ');
 			++*l;
-			t_object sp = sphere(tmp);
+			write(2, "C",1);
+			if (tmp[0][0] == '0' && o < s->obj_len)
+			{
+				s->objects[o] = sphere(tmp);
+				++o;
+				printf("%d\n", o);
+			}
 			printf("A\n");
-			sp.inter(NULL, NULL);
+			s->objects[0].inter(NULL, NULL);
 		}
 	printf("%d\n", *l);
 }
 
 static void    camera_parse(t_mlx *s, int fd, int *l)
 {
-    char        *line;
-    char        **tmp;
+	char        *line;
+	char        **tmp;
 
-    while (*l != 2 && get_next_line(fd, &line) > 0)
-        if (line[0] != '#')
-        {
-            tmp = ft_strsplit(line, ' ');
+	while (*l != 2 && get_next_line(fd, &line) > 0)
+		if (line[0] != '#')
+		{
+			tmp = ft_strsplit(line, ' ');
 			s->cam.x = ft_atoi(tmp[0]);
 			s->cam.y = ft_atoi(tmp[1]);
 			s->cam.z = ft_atoi(tmp[2]);
@@ -47,28 +55,28 @@ static void    camera_parse(t_mlx *s, int fd, int *l)
 			s->cam.rot_y = ft_atoi(tmp[5]);
 			s->cam.rot_z = ft_atoi(tmp[6]);
 			free(line);
-        }
-    printf("%d\n", s->cam.rot_y);
+		}
+	printf("%d\n", s->cam.rot_y);
 	body_parse(s, fd, l);
 }
 
 void            parse(t_mlx *s, int fd) {
-    int         l;
-    char        *line;
-    char        **tmp;
+	int         l;
+	char        *line;
+	char        **tmp;
 
-    l = 0;
-    while (l != 1 && get_next_line(fd, &line) > 0) 
-        if (line[0] != '#') 
-        {
-            tmp = ft_strsplit(line, '/');
-            s->src_len = ft_atoi(tmp[0]);
-            s->obj_len = ft_atoi(tmp[1]);
-            s->sources = (t_object *)malloc(sizeof(t_object) * s->src_len);
-            s->objects = (t_object *)malloc(sizeof(t_object) * s->obj_len);
-            ++l;
-            printf("%s\n", line);
+	l = 0;
+	while (l != 1 && get_next_line(fd, &line) > 0) 
+		if (line[0] != '#') 
+		{
+			tmp = ft_strsplit(line, '/');
+			s->src_len = ft_atoi(tmp[0]);
+			s->obj_len = ft_atoi(tmp[1]);
+			s->sources = (t_object *)malloc(sizeof(t_object) * s->src_len);
+			s->objects = (t_object *)malloc(sizeof(t_object) * s->obj_len);
+			++l;
+			printf("%s\n", line);
 			free(line);
-        }
-    camera_parse(s, fd, &l);
+		}
+	camera_parse(s, fd, &l);
 }
