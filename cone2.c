0d18245b114ed74ed8f8596fc38935eecc19323a
dbreton <dbreton@student.42.fr>
2017-02-02 12:32:56 +0100

    normified cone

--- cone2.c
+++ cone2.c
@@ -3,15 +3,26 @@
 //
 #include "fractol.h"
 
-static char			validate_direction(t_vector *io, t_vector *norm, double rad)
+static char			validate_direction(t_vector *io,
+										  t_vector *norm,
+										  t_object *self, t_vector *piv)
 {
 	double 			est_tan;
 	double 			exp_tan;
 
 
 	est_tan = norm->length / io->length;
-	exp_tan = tan(rad);
-	return (!almost_equal_relative(est_tan, exp_tan));
+	exp_tan = tan(self->radius);
+	if(!almost_equal_relative(est_tan, exp_tan))
+	{
+		*piv = mult_vec_double(self->dir,
+							-1.0f * io->length / cos(self->radius));
+		piv->x += self->x;
+		piv->y += self->y;
+		piv->z += self->z;
+		return (0);
+	}
+	return (1);
 }
 
 t_vector			cone_normal(t_vector *inter, t_object *self)
@@ -32,16 +43,8 @@ t_vector			cone_normal(t_vector *inter, t_object *self)
 	piv.z += self->z;
 	norm = sub_vec_by_vec(*inter, piv);
 	comp_vector_length(&norm);
-
-	if (validate_direction(&io, &norm, self->radius) != 0)
-	{
-		write(1, "A\n", 2);
-		piv = mult_vec_double(self->dir, -1.0f * pi);
-		piv.x += self->x;
-		piv.y += self->y;
-		piv.z += self->z;
+	if (!validate_direction(&io, &norm, self, &piv))
 		norm = sub_vec_by_vec(*inter, piv);
-	}
 	normalize_vector(&norm);
 	return (norm);
 }
