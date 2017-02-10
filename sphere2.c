b7a2926082ac9494d5eaedef9df2882b6685ba48
dbreton <dbreton@student.42.fr>
2017-02-10 12:31:28 +0100

    added working specular lighting

--- sphere2.c
+++ sphere2.c
@@ -53,16 +53,13 @@ t_color			sphere_specular(t_object *src, t_object *self,
 	vecs[0].y = src->y - inter->y;
 	vecs[0].z = src->z - inter->z;
 	normalize_vector(&vecs[0]);
-	vecs[3] = sub_vec_by_vec(*v, *inter);
+	vecs[3] = sub_vec_by_vec(*inter, *v);
+	normalize_vector(&vecs[3]);
 	l_dot_normal = dot(&vecs[1], &vecs[0]);
 	if (l_dot_normal > 0.0f)
 	{
-		vecs[2] = sub_vec_by_vec(mult_vec_double(
-			vecs[1], 2 * dot(&vecs[1], &vecs[3])), vecs[0]);
-		tmp = mult_color_double(mult_color_double(src->color,
-												  self->ks *
-												  pow(dot(&vecs[2], &vecs[3]), self->psh)),
-								src->intensity / (log(vecs[0].length)));
+		vecs[2] = sub_vec_by_vec(mult_vec_double(vecs[1], 2.0f * dot(&vecs[0], &vecs[1])),vecs[0]);
+		tmp = mult_color_double(src->color, pow(dot(&vecs[2], &vecs[3]), self->psh));
 	}
 	else
 		tmp = create_color("0,0,0");
